260a99fc5c6f83a0d3ef81a7d46969ed11f335fe
neto <hatreun@gmail.com>
2014-03-13 16:19:50 -0600

    Adding stuff User

--- src/main/scala/code/model/User.scala
+++ src/main/scala/code/model/User.scala
@@ -12,20 +12,24 @@ case class User(id: Option[Int], name: String, uid: String, profileImage: String
 
 class Users(tag: Tag) extends Table[User](tag, "USERS") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
   def name = column[String]("name")
+
   def uid = column[String]("uid")
+
   def profileImage = column[String]("profile_image")
 
-  def * = (id.?, name, uid, profileImage) <> (User.tupled, User.unapply)
+  def * = (id.?, name, uid, profileImage) <>(User.tupled, User.unapply)
 }
 
-object User extends ((Option[Int], String, Stringm String)=>User){
+object User extends ((Option[Int], String, String, String) => User) {
   val users = TableQuery[Users]
   val db = DataBase.db
-  def find(uid: String): Option[String] = {
-    db.withSession{
-      implicit session =>
 
+  def find(uid: String): Option[User] = {
+    db.withSession {
+      implicit session =>
+        users.filter(_.uid === uid).firstOption
     }
   }
 }
